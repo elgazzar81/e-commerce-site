b7636af92bab8fea5f681f36f8c7f6b6415ea61b
Othmane ELGAZZAR <elgazzarothmane@gmail.com>
2025-07-09 15:38:48 +0100

    changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logoImg      from '../../assets/logo_website-removebg-preview.png';
 import shoppingCart from '../../assets/shopping.png';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
+  const [menuOpen, setmenuOpen]=useState(false);
   return (
     <nav className="navbar">
       <img src={logoImg}      alt="Logo"    className="logo" />
-      <ul>
+      <button className='hamburger'
+      onClick={()=>setmenuOpen(open => !open)}
+      aria-label='Toggle Navigation'>
+       {menuOpen ?<FaTimes size={24}/> :<FaBars size={24}/>}
+      </button>
+      <ul className={menuOpen ? 'nav-links open': 'nav-links'}>
         <li>Home</li>
         <li>Shop</li>
         <li>Pages</li>
